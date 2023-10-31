a60eeed661aa27a0f39d3f237146becaf93ce047
mohammed shousha <mshousha29@gmail.com>
2023-10-31 06:55:28 -0700

    updated 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,12 +1,11 @@
 #include "binary_trees.h"
-#include <cmath>
 
 /**
  * calcHeight - calcHeight of the given binary tree
  *
  * @tree: pointer to the root node of the tree to measure the height
  * @counter: counter to calculate the height and store the height in it
- * 
+ *
  * Return: the counter that has the value of the height
 */
 size_t calcHeight(const binary_tree_t *tree, size_t counter)
@@ -21,15 +20,15 @@ size_t calcHeight(const binary_tree_t *tree, size_t counter)
 
 	leftValue = calcHeight(tree->left, counter + 1);
 	rightValue = calcHeight(tree->right, counter + 1);
-	return (max(leftValue, rightValue));
+	return ((leftValue > rightValue)? leftValue : rightValue);
 }
 
 /**
  * binary_tree_height - the main function
  * to calculate the height of the binary tree
- * 
+ *
  * @tree: pointer to the root node of the tree to measure the height
- * 
+ *
  * Return: the height of the binary tree, otherwise 0
 */
 size_t binary_tree_height(const binary_tree_t *tree)
