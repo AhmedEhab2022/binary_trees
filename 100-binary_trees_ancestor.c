1a81c1798704b4b92b224e4cde0ef74265db9128
mohammed shousha <mshousha29@gmail.com>
2023-11-02 02:34:50 -0700

    update 100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	binary_tree_t *first_parent, *second_parent;
 
 	if (first == NULL || second == NULL)
-		return NULL;
+		return (NULL);
 
 	if (first == second)
 		return ((binary_tree_t *)first);
@@ -22,10 +22,12 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	first_parent = first->parent;
 	second_parent = second->parent;
 
-	if (first_parent == NULL || first == second_parent)
+	if (first_parent == NULL || first == second_parent ||
+		(!first_parent->parent && second_parent))
 		return (binary_trees_ancestor(first, second_parent));
 
-	if (second_parent == NULL || first_parent == second)
+	if (second_parent == NULL || first_parent == second ||
+		(!second_parent->parent && first_parent))
 		return (binary_trees_ancestor(first_parent, second));
 
 	return (binary_trees_ancestor(first_parent, second_parent));
